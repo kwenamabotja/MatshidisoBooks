18a4a90f3787bcf6600eaac03f17921d9a0115b0
Patrick <patrick@tangentsolutions.co.za>
2021-10-23 15:19:09 +0200

    setting up base template

--- main/models.py
+++ main/models.py
@@ -5,3 +5,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    """Home page."""
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        return context
