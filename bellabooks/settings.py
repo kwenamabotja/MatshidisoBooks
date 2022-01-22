d54af74027a5b1c26a9fc42204785665a5dccbb7
Patrick <patrick@tangentsolutions.co.za>
2022-01-22 15:40:13 +0200

    adding host

--- bellabooks/settings.py
+++ bellabooks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4r8kl$(&*t_i@g*1qx8x2ugx6@m0tl-x&t)^o$8e2k&tp-v37&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "178.128.171.157"]
 
 # Application definition
 
